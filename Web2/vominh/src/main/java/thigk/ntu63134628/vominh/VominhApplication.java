36e4d7ea454ccae91395cb4ad257ffc2d053bb18
Araxys123 <vminha52020@gmail.com>
2024-03-26 15:11:34 +0700

    up

---
+++ Web2/vominh/src/main/java/thigk/ntu63134628/vominh/VominhApplication.java
@@ -0,0 +1,13 @@
+package thigk.ntu63134628.vominh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VominhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VominhApplication.class, args);
+	}
+
+}
---
+++ Web2/vominh/src/test/java/thigk/ntu63134628/vominh/VominhApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk.ntu63134628.vominh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VominhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
